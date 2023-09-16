48cded56b36e92074dbb72ff7ff6d60757c0bb96
Lucky-Star-999 <theboost1305@gmail.com>
2023-09-16 15:27:18 +0700

    Swagger + ResponseEntity

--- src/main/java/com/example/v2basicconcept/controller/HelloController.java
+++ src/main/java/com/example/v2basicconcept/controller/HelloController.java
@@ -1,12 +1,16 @@
 package com.example.v2basicconcept.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Tag(name = "Hello Controller", description = "The first controller!")
 public class HelloController {
 
     @GetMapping("/hello")
+    @Operation(summary = "hello", description = "The first method!")
     public String hello() {
         return "Hello Newcomers!";
     }
--- src/main/java/com/example/v2basicconcept/controller/MonthController.java
+++ src/main/java/com/example/v2basicconcept/controller/MonthController.java
@@ -1,6 +1,11 @@
 package com.example.v2basicconcept.controller;
 
 import com.example.v2basicconcept.service.MonthService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -8,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/month")
+@Tag(name = "Month Controller", description = "Month Controller!")
 public class MonthController {
 
     private final MonthService monthService;
@@ -17,7 +23,14 @@ public class MonthController {
     }
 
     @GetMapping("/get-month-from-number")
-    public String getMonthFromNumber(@RequestParam int number) {
-        return monthService.getMonthFromNumber(number);
+    @Operation(summary = "getMonthFromNumber", description = "Get month from number")
+    public ResponseEntity<String> getMonthFromNumber(
+            @Parameter(description = "Fill in from 1 to 12") @RequestParam int number) {
+        String month = monthService.getMonthFromNumber(number);
+        if (month.equals("")) {
+            // If it lacks body(), it will need build()
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(month);
     }
 }
