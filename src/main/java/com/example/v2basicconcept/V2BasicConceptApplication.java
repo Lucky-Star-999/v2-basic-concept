78e6bb008f913056b620f268984486edbe954298
Lucky-Star-999 <theboost1305@gmail.com>
2023-09-16 13:46:45 +0700

    Init

---
+++ src/main/java/com/example/v2basicconcept/V2BasicConceptApplication.java
@@ -0,0 +1,13 @@
+package com.example.v2basicconcept;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class V2BasicConceptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(V2BasicConceptApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/v2basicconcept/V2BasicConceptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.v2basicconcept;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class V2BasicConceptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
